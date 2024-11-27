98163a7fda830293524afa98d1bb522890a2e985
Marilyn Negrete <marilyn.negrete@globant.com>
2024-11-27 11:02:35 -0600

    fix(ghPages): rename homepage path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./layout/Nav";
 import Home from "./__views__/home/Home.js";
 import SearchResults from "./__views__/news/SearchResults";
@@ -12,30 +11,29 @@ import "./__styles__/main.scss";
 const App = () => {
   const [sections, setSections] = useState([]);
   const fetchData = async () => {
-    const key = process.env.REACT_APP_NYT_API_KEY
+    const key = process.env.REACT_APP_NYT_API_KEY;
     const res = await fetch(
       `${process.env.REACT_APP_NYT_URL}/svc/news/v3/content/section-list.json?api-key=${key}`
     );
     const response = await res.json();
     setSections(response.results);
-  }
+  };
 
   useEffect(() => {
-   fetchData();
+    fetchData();
   }, []);
 
   return (
-    <>
-      <BrowserRouter>
-        <Nav sections={sections} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/section/:id" element={<Category />} />
-          <Route path="/search-results" element={<SearchResults />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <Router>
+      <Nav sections={sections} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/section/:id" element={<Category />} />
+        <Route path="/search-results" element={<SearchResults />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
